cbdf11b5800de80a9dee677635c1ac947ef7a49d
MUSICNFTACCESS <crimzncipriano@gmail.com>
2025-05-02 16:19:21 -0400

    Fix: /chat route added properly

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -16,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
-// Serve homepage
+// Homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
