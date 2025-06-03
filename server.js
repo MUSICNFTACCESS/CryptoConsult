98aa42f60b9d624d82645e3a96bc7a31e059f834
MUSICNFTACCESS <crimzncipriano@gmail.com>
2025-06-03 16:56:14 -0400

    🚨 Hotfix: Restore index.html route at / root

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const openai = new OpenAI({
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+// ✅ Serve index.html at root
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
