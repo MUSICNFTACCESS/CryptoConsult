a761ff486f6f670d631069f6172c72b4ce44e847
MUSICNFTACCESS <crimzncipriano@gmail.com>
2025-06-03 23:26:45 -0400

    🛠️ Add global error handler to debug silent crash

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+process.on("uncaughtException", err => console.error("❌ Uncaught:", err));
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
